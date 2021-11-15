5ef21f5cafcf0927e3a3fed4b30595c3e814e77d
Riley <increaseblue666@gmail.com>
2021-11-15 22:13:18 +0900

    Feat : unit test loadData
    - api key 받아오는지 테스트

--- src/trials/trials.service.spec.ts
+++ src/trials/trials.service.spec.ts
@@ -1,18 +1,38 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TrialsService } from './trials.service';
 
 describe('TrialsService', () => {
   let service: TrialsService;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TrialsService],
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env',
+          isGlobal: true,
+        }),
+      ],
+      providers: [TrialsService, ConfigService],
     }).compile();
 
     service = module.get<TrialsService>(TrialsService);
+    configService = module.get<ConfigService>(ConfigService);
   });
 
-  it('should be defined', () => {
+  it('TrialService should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('ConfigService should be defined', () => {
+    expect(configService).toBeDefined();
+  });
+
+  describe('loadData', () => {
+    it('should have API_KEY_AUTH', () => {
+      const apiKey = configService.get('API_KEY_AUTH');
+      expect(apiKey).not.toBeUndefined();
+    });
   });
 });
