481d8c9303dcb186666dfe1487322ca99ad4e99c
Joseph <josephkim828@gmail.com>
2021-11-16 20:24:43 +0900

    Modify: trial entity prettier update

--- src/trials/entities/trial.entity.ts
+++ src/trials/entities/trial.entity.ts
@@ -1,39 +1,43 @@
-import { Optional } from '@nestjs/common'
-import { Column, CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Trial {
+  @PrimaryColumn()
+  id: string; // 과제번호
 
-    @PrimaryColumn()
-    id: string // 과제번호
+  @Column()
+  title: string; // 과제명
 
-    @Column()
-    title: string // 과제명
-    
-    @Column({ nullable: true })
-    department: string // 진료과
-    
-    @Column({ nullable: true })
-    institution: string // 연구책임기관
-    
-    @Column({ nullable: true })
-    subjectCount: number // 전체목표연구대상자수
-    
-    @Column({ nullable: true })
-    period: string // 연구기간
-    
-    @Column({ nullable: true })
-    researchType: string // 연구종류
-    
-    @Column({ nullable: true })
-    stage: string // 임상시험단계(연구모형)
-    
-    @Column({ nullable: true })
-    scope: string // 연구범위
-    
-    @CreateDateColumn()
-    createdAt: Date
-    
-    @UpdateDateColumn()
-    updatedAt: Date
+  @Column({ nullable: true })
+  department: string; // 진료과
+
+  @Column({ nullable: true })
+  institution: string; // 연구책임기관
+
+  @Column({ nullable: true })
+  subjectCount: number; // 전체목표연구대상자수
+
+  @Column({ nullable: true })
+  period: string; // 연구기간
+
+  @Column({ nullable: true })
+  researchType: string; // 연구종류
+
+  @Column({ nullable: true })
+  stage: string; // 임상시험단계(연구모형)
+
+  @Column({ nullable: true })
+  scope: string; // 연구범위
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
